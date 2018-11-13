2b0813e22e0e04b9a86635874877921d278cc09e
Justin McCoy <sholmes@Justins-MacBook-Pro.local>
2018-11-13 08:56:56 -0500

    Changed app for new API interfaces to POST

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ console.log(' ');
 			headers: {
 			   Authorization: 'Bearer ' + token			 
 			},
-			qs: { instance_id: env.instance_id, deployment_id: env.deployment_id, published_model_id: env.published_model_id },	
+			qs: { },	
 			json: req.body.input
 		};
 		request(opts, function(err, r, body) {
